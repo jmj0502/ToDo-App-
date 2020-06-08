5315e35b68484bdadc8ad6004a4f2f6cfd70173c
jmj0502 <juarezmachadojorgemartin@gmail.com>
2020-06-08 12:43:59 -0400

    Some bugs were fixed

--- src/db.js
+++ src/db.js
@@ -1,11 +1,14 @@
 //here we are going to setup our mongoose configuration.
 const mongoose = require('mongoose');
 
+console.log(process.env.DB_URI);
+
 //connecting to the db.
 mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(db => console.log('Connected to the db!'));
+}).then(db => console.log('Connected to the db!'))
+.catch(err => console.log(err));
 
 //Setting a little mongoose config that will allow us to user certain methods.
 mongoose.set('useFindAndModify', false);
